929c53652f8b9a71fe7e869b8656e423c94ea3f1
alffox <alfonso.crisci.it@gmail.com>
2020-05-06 12:07:33 +0900

    LRSUPPORT-37659 Add a code button to the knowledge base editor

--- src/knowledge.ts
+++ src/knowledge.ts
@@ -1,3 +1,59 @@
+function addArticleCodeButton(toolbarContainer : HTMLDivElement) : void {
+  // Gets the buttons toolbar
+  var toolbar = <HTMLDivElement> toolbarContainer.querySelector('.ssc-view-3df91d6a.ssc-group-f69f19c1');
+
+  // Creates the code format container button
+  var codeFormatButton = document.createElement('div');
+  codeFormatButton.classList.add('ssc-view-3df91d6a', 'src-components-EditorToolbar-ToolbarButton---button---2IfvR');
+  codeFormatButton.setAttribute('tabindex', '0');
+  codeFormatButton.setAttribute('role', 'button');
+  codeFormatButton.setAttribute('id', 'custom-code-format-button');
+  codeFormatButton.setAttribute('data-test-id', 'toolbarCodeFormatButton');
+
+  // Creates the code format label
+  var codeFormatLabel = document.createElement('div');
+  codeFormatLabel.classList.add('src-components-EditorToolbar-ToolbarButton---label---PACxZ');
+  codeFormatLabel.setAttribute('title', 'Code Format');
+
+  // Creates the code format icon
+  var codeFormatIcon = document.createElement('img');
+  codeFormatIcon.setAttribute('src', 'https://www.tiny.cloud/docs/images/icons/code-sample.svg'); // Icon taken from https://www.tiny.cloud/docs/advanced/editor-icon-identifiers/
+  codeFormatIcon.setAttribute('alt', "code format")
+
+  // Adds icon to the label
+  codeFormatLabel.appendChild(codeFormatIcon);
+
+  // Adds the label to the button
+  codeFormatButton.appendChild(codeFormatLabel);
+
+  // Adds the button to the toolbar
+  var toolbarPreButton = <HTMLDivElement> toolbar.querySelector('div[data-test-id="toolbarPreButton"]');
+  toolbar.insertBefore(codeFormatButton, toolbarPreButton);
+
+  // Registers the button functionality
+  // API: https://www.tiny.cloud/docs/api/tinymce/tinymce.formatter/
+  tinymce.activeEditor.formatter.register('codeformat', {
+    inline: 'code'
+  });
+
+  // Adds function to the button
+  codeFormatButton.addEventListener('click', function(e) {
+    var target = <HTMLElement> e.currentTarget;
+    tinymce.activeEditor.focus();
+    tinymce.activeEditor.formatter.toggle('codeformat');
+    tinymce.DOM.toggleClass(target, 'src-components-EditorToolbar-ToolbarButton---active---3qTSV');
+  })
+
+  // Adds event listener to check <code> markup everywhere on the active editor
+  tinymce.activeEditor.on('click', function(e) {
+    if ((tinymce.activeEditor.selection.getNode().nodeName) == "CODE") {
+      codeFormatButton.classList.add('src-components-EditorToolbar-ToolbarButton---active---3qTSV');
+    } else {
+      codeFormatButton.classList.remove('src-components-EditorToolbar-ToolbarButton---active---3qTSV');
+    }
+  });
+}
+
 function addArticleFormattingButtons() : void {
   var toolbarContainers = <Array<HTMLDivElement>> Array.from(document.querySelectorAll('div[class*="ssc-container-85be2f31 src-components-EditorToolbar-index---bar---"]'));
 
@@ -9,5 +65,7 @@ function addArticleFormattingButtons() : void {
     }
 
     toolbarContainer.classList.add('lesa-ui-stackedit');
+
+    addArticleCodeButton(toolbarContainer);
   }
 }
