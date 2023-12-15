52d0db65c4a118602d1aa907da4723e4799a8b32
Jorge Díaz <jorge.diaz@liferay.com>
2023-12-15 16:37:41 +0100

    Add 'edit' link to provisioning.liferay.com to be able to edit the notes easily

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -134,6 +134,12 @@ function addOrganizationField(
 
   var notesItems = [];
 
+  if (organizationInfo) {
+    var provisioningSupportInstructionsLink = createAnchorTag(
+    "edit", "https://provisioning.liferay.com/group/guest/~/control_panel/manage?p_p_id=com_liferay_osb_provisioning_web_portlet_AccountsPortlet&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_com_liferay_osb_provisioning_web_portlet_AccountsPortlet_mvcRenderCommandName=%2Faccounts%2Fview_account&_com_liferay_osb_provisioning_web_portlet_AccountsPortlet_tabs1=support&_com_liferay_osb_provisioning_web_portlet_AccountsPortlet_accountKey=" + organizationInfo.organization_fields.account_key);
+    notesItems.push(provisioningSupportInstructionsLink);
+  }
+
   if (organizationInfo && organizationInfo.notes) {
     var notesContainer = document.createElement('div');
     notesContainer.textContent = organizationInfo.notes;
