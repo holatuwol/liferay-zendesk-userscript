cfdd2760b3bee99ff751eb535cd4afc6761f2643
Jorge Díaz <jorge.diaz@liferay.com>
2025-01-14 17:48:53 +0100

    Add check for premium_service organization tag that is added to customers with trial TAM Services

--- src/priority.ts
+++ src/priority.ts
@@ -184,6 +184,7 @@ function addCustomerTypeMarker(
 ) : void {
 
   addOrganizationTagSearchHeader(priorityElement, organizationTagSet, 'tam_services', 'TAM Services', 'priority-critical');
+  addOrganizationTagSearchHeader(priorityElement, organizationTagSet, 'premium_service', 'Trial TAM Services', 'priority-critical');
 
   addOrganizationTagSearchHeader(priorityElement, organizationTagSet, 'gs_opportunity', 'GS Opportunity', 'priority-minor');
   addOrganizationTagSearchHeader(priorityElement, organizationTagSet, 'service_solution', 'Service Portal Customer', 'priority-minor');
